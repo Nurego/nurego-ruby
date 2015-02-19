b6f168e59d05cb30a49c8a3c4320a7c9ad60673b
anatb <anat.birnboim@openskymarkets.com>
2015-02-18 23:07:10 -0800

    add spec

--- spec/integration/registrations_spec.rb
+++ spec/integration/registrations_spec.rb
@@ -2,15 +2,25 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Registrations" do
   before(:all) do
-    EMAIL = "integration.test+#{UUIDTools::UUID.random_create.to_s}@openskymarkets.com"
 
     setup_nurego_lib(true)
   end
 
+  before do
+    @email = "integration.test+#{UUIDTools::UUID.random_create.to_s}@openskymarkets.com"
+  end
+
   it "can register a subscriber" do
-    registration = Nurego::Registration.create({email: EMAIL})
+    registration = Nurego::Registration.create({email: @email})
     customer = registration.complete(id: registration.id, password: EXAMPLE_PASSWORD)
-    customer["email"].should == EMAIL
+    customer["email"].should == @email
+    customer["object"].should == "customer"
+  end
+
+  it "can register a subscriber providing email only on complete" do
+    registration = Nurego::Registration.create()
+    customer = registration.complete(id: registration.id, password: EXAMPLE_PASSWORD, email: @email)
+    customer["email"].should == @email
     customer["object"].should == "customer"
   end
 end
