321715540b4a686a2dae4f4477461ba6dd1a47e1
anatb <anat.birnboim@openskymarkets.com>
2015-03-05 06:16:21 -0800

    add legal docs public api

---
+++ lib/nurego/legal_doc.rb
@@ -0,0 +1,18 @@
+module Nurego
+  class LegalDoc < APIResource
+
+    def self.retrieve(params = {}, api_key = nil)
+      raise NotImplementedError.new("Docs cannot be retrieved with ID. Retrieve a legal document using search parameters") if
+          !params.is_a?(Hash)  || params[:id]
+
+      response, api_key = Nurego.request(:get, self.url, api_key, params)
+      Util.convert_to_nurego_object(response, api_key)
+    end
+
+    def url
+      puts "URL #{LegalDoc.url}"
+      LegalDoc.url
+    end
+
+  end
+end
