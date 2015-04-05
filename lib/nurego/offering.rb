5dc39618455a196968e6d79b67756b6de3c31801
Bar-Jakubovitz <bar.jakubovitz@gmail.com>
2015-04-05 16:12:44 +0300

    Create a new as_cf_catalog method. return a Json following the CF service broker API catalog scheme.

--- lib/nurego/offering.rb
+++ lib/nurego/offering.rb
@@ -1,4 +1,5 @@
 module Nurego
+
   class Offering < APIResource
 
     def self.retrieve(id, api_key = nil)
@@ -8,6 +9,11 @@ module Nurego
     def self.current(params = {}, api_key = nil)
       response, api_key = Nurego.request(:get, self.url, api_key, params)
       Util.convert_to_nurego_object(response, api_key)
+    end
+
+    def self.as_cf_catalog(url, api_key)
+      response, api_key = Nurego.request(:get, url, api_key, {:distribution_channel => 'cf'})
+      Util.convert_to_nurego_object(response, api_key).to_json
     end
 
     def plans
