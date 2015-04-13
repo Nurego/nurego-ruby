3567c320a9a1707fb42c2602bae736545a133517
Bar-Jakubovitz <bar.jakubovitz@gmail.com>
2015-04-13 11:53:09 +0300

    Implemented parsing to CF catalog.

--- lib/nurego/offering.rb
+++ lib/nurego/offering.rb
@@ -11,15 +11,63 @@ module Nurego
       Util.convert_to_nurego_object(response, api_key)
     end
 
-    def self.as_cf_catalog(url, api_key)
-      Nurego.api_base = url
+    def self.as_cf_catalog(url, api_key )
+      Nurego.api_base = url if url
       response, api_key = Nurego.request(:get, self.url, api_key, {:distribution_channel => 'cf'})
-      Util.convert_to_nurego_object(response, api_key).to_json
-      # todo Add Parsing to catalog format
+      offering_to_broker_catalog(Util.convert_to_nurego_object(response, api_key)).to_json
     end
 
     def plans
       Plan.all({:offering => id }, @api_key)
+    end
+
+private
+
+    def self.offering_to_broker_catalog(nurego_offering)
+      cf_offer = {
+          offer_id: nurego_offering['id'],
+          offer_name: nurego_offering['name'],
+          offer_description: nurego_offering['description'],
+          :services => []
+      }
+
+      cf_service = {
+          # required
+          id: nurego_offering['service_id'],
+          name: nurego_offering['service_name'],
+          description: nurego_offering['service_description'],
+          bindable: true,
+          plans: []
+
+          ## possible
+          # tags: [],
+          # metadata: Object,
+          # requires: [],
+          # plan_updateable: true,
+          # dashboard_client: Object {id,secret,redirect_uri}
+
+      }
+
+      nurego_offering['plans']['data'].each do | nurego_plan |
+        cf_plan = {
+            # required
+            id: nurego_plan['id'],
+            name: nurego_plan['name'],
+            description: nurego_plan['description'],
+
+            ## possible
+            # metadata: Object,
+            # free: true,
+        }
+
+        recurring = nurego_plan['features']['data'].find { | feature | feature['element_type'] == 'recurring' }
+        cf_plan[:free] = !(recurring && recurring['price'] > 0)
+
+        cf_service[:plans] << cf_plan # Add plans to the service
+      end
+      cf_offer[:services] << cf_service # Add service to offer
+
+      cf_offer
     end
   end
 end
