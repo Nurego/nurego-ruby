3cc630697217047930e509943f04600f8a8ed9b0
Bar-Jakubovitz <bar.jakubovitz@gmail.com>
2015-04-19 14:53:25 +0300

    Removed duplicate test "can fetch the plans from offering". Added test for cloud foundry catalog parsing.

--- spec/integration/offerings_spec.rb
+++ spec/integration/offerings_spec.rb
@@ -2,14 +2,13 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Offerings" do
   before(:all) do
-    setup_nurego_lib
-    setup_login_and_login
+    setup_nurego_lib(true, true) # no_register = true + public_key = true
   end
 
   it "can fetch current offering" do
     offering = Nurego::Offering.current
     offering["object"].should == "offering"
-
+    offering.plans["object"].should == "list"
     offering.plans.each do |plan|
       plan["object"].should == "plan"
       plan.features.each do |feature|
@@ -30,17 +29,34 @@ describe "Offerings" do
     end
   end
 
-  it "can fetch the plans from offering" do
+  it "can parse current offering to cloud foundry catalog" do
     offering = Nurego::Offering.current
-    plans = offering.plans
-    plans["object"].should == "list"
+    offer_as_catalog = offering.to_cloud_foundry_catalog
+    response_json = JSON.parse offer_as_catalog
 
-    plans.each do |plan|
-      plan["object"].should == "plan"
-      plan.features.each do |feature|
-        feature["object"].should == "feature"
+    expect(response_json.keys).to include("services")
+
+    services = response_json["services"]
+    expect(services.length).to be > 0
+
+    services.each do |service|
+      expect(service.keys.length).to be >= 5
+      expect(service.keys).to include("id")
+      expect(service.keys).to include("name")
+      expect(service.keys).to include("description")
+      expect(service.keys).to include("bindable")
+      expect(service.keys).to include("plans")
+
+      plans = service["plans"]
+      expect(service.keys.length).to be >= 5
+
+      expect(plans.length).to be > 0
+      plans.each do |plan|
+        expect(plan.keys.length).to be >= 3
+        expect(plan.keys).to include("id")
+        expect(plan.keys).to include("name")
+        expect(plan.keys).to include("description")
       end
     end
   end
-
 end
