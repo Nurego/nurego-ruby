cfe2d180e9a3e0a9cda9337c00b4c3429e770e35
Bar-Jakubovitz <bar.jakubovitz@gmail.com>
2015-04-19 15:26:19 +0300

    Added logout to be able to run in integration tests.

--- spec/integration/registrations_spec.rb
+++ spec/integration/registrations_spec.rb
@@ -2,8 +2,8 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Registrations" do
   before(:all) do
-
     setup_nurego_lib(true)
+    Nurego::Auth.logout
   end
 
   before do
