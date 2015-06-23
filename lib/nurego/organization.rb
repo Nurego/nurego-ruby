2bd795a4fb04487050a33bd6c2c3b361e0aa174f
Meital Federgreen <zask.mz@gmail.com>
2015-06-23 21:29:09 +0300

    Update trial period

--- lib/nurego/organization.rb
+++ lib/nurego/organization.rb
@@ -38,5 +38,10 @@ module Nurego
       Util.convert_to_nurego_object(response, api_key)
     end
 
+    def update_trial_period(params = {}, api_key = nil)
+      response, api_key = Nurego.request(:post, url + '/update_trial_period', api_key, params)
+      Util.convert_to_nurego_object(response, api_key)
+    end
+
   end
 end
